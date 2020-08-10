4060bef85b9b4017ceeb4231ec0902ae3ba48c43
Victor Martins Machado <victormachado90@gmail.com>
2020-08-10 20:29:56 -0300

    Alteração da rota de criação de usuário

--- src/routes.ts
+++ src/routes.ts
@@ -3,8 +3,6 @@ import { createUserController } from "./useCases/CreateUser";
 
 const router = Router();
 
-router.post('/users', (req, res) => {
-  return createUserController.handle(req, res)
-})
+router.post('/users', createUserController.handle)
 
 export { router }
